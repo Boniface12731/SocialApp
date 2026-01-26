64ced58f4d9ffcb631180faa162a3251cf2d229d
Bonface Osuka <bonyosuks12731@gmail.com>
2026-01-26 10:04:06 +0300

    profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,6 @@ import { dummyPostsData, dummyUserData } from '../assets/assets'
 import Loading from '../components/Loading'
 import UserProfileInfo from '../components/UserProfileInfo'
 
-
 const Profile = () => {
   const {profileId} = useParams()
   const [user, setUser] = useState(null)
@@ -33,14 +32,23 @@ const Profile = () => {
 
                  {/* User Info */}
                    <UserProfileInfo user={user} posts={posts} profileId={profileId} setShowEdit={setShowEdit}/>
-
-                
             </div>
+
+             {/* Tabs */}
+          <div className='mt-6'>
+            <div className='bg-white rounded-xl shadow p-1 flex max-w-md mx-auto'>
+              {
+                ["posts", "media", "likes"].map((tab) =>(
+                  <button onClick={()=> setActiveTab(tab)} key={tab} className={`flex-1 px-4 py-2 text-sm font-medium rounded-lg transition-colors cursor-pointer ${activeTab === tab ? "bg-indigo-600 text-white" : "text-gray-600 hover:text-gray-900"}`}>
+                  {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                  </button>
+               ))}
+            </div>
+          </div>
       </div>
     </div>
   )
   :
   (<Loading/>)
-
 }
 export default Profile
